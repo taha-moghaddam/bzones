b25edf0a9246a4ec9223739e4403408a1360ba1b
mtmoghaddam <m5taha5m@gmail.com>
2023-04-22 07:03:13 +0330

    refactor: Set default db-prefix for tables

--- database/migrations/2023_04_18_012237_create_zones_table.php
+++ database/migrations/2023_04_18_012237_create_zones_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create(config('admin.extensions.bzones.config.db-prefix') . 'zones', function (Blueprint $table) {
+        Schema::create(config('admin.extensions.bzones.config.db-prefix', 'bzones_') . 'zones', function (Blueprint $table) {
             $table->id();
             $table->string('name', 150);
 
             $table->nestedSet();
-            
+
             $table->timestamps();
             $table->softDeletes();
         });
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists(config('admin.extensions.bzones.config.db-prefix') . 'zones');
+        Schema::dropIfExists(config('admin.extensions.bzones.config.db-prefix', 'bzones_') . 'zones');
     }
 };

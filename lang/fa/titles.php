545874b74eb4b5114de196f88b19dddea0858bfd
mtmoghaddam <m5taha5m@gmail.com>
2023-04-22 07:03:27 +0330

    bug: Fix route problem

--- lang/fa/titles.php
+++ lang/fa/titles.php
@@ -3,6 +3,7 @@
 return [
     'Id' => '#',
     'Name' => 'نام',
+    'Zones' => 'مناطق',
     'Created at' => 'زمان ثبت',
     'Updated at' => 'آخرین ویرایش',
     'Deleted at' => 'زمان حذف',
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Routing\Router;
 use Bikaraan\BZones\Http\Controllers\ZonesController;
+use Bikaraan\BZones\Http\Controllers\SearchController;
 
 Route::group([
     'prefix' => config('admin.extensions.bzones.config.prefix', 'bzones'),
     'as' => 'bzones.',
 ], function (Router $router) {
 
-    $router->get('/', BZonesController::class . '@index');
-
     $router->resource('zones', ZonesController::class);
+
+    $router->get('search', SearchController::class);
 });
